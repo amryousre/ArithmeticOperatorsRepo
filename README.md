1ad971dd6ade1e2b4fc93596a970112c53a054d8
Amr Yousre <master_j_20@hotmail.com>
2021-03-22 17:49:26 +0200

    Practice For Operators

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ArithmeticOperatorsRepo
- practice for ArithmeticOperators
+A simple command-line application.
+
+Created from templates made available by Stagehand under a BSD-style
+[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
---
+++ bin/ArithmeticOperators.dart
@@ -0,0 +1,17 @@
+import 'dart:io';                   //Import Taking Input From User Libraries
+void main() {
+print('Please Enter Number1:');     //asking the user to enter the first number
+int number1 = int.parse(stdin.readLineSync());  //putting the value that user entered in number1
+print('Please Enter Number2:');     //asking the user to enter the first numbe2
+int number2 = int.parse(stdin.readLineSync());  //putting the value that user entered in number2
+int sum = number1+number2;          //adding the two values in sum to print it
+int sub = number1-number2;          //adding the two values in subtract to print it
+double div = number1/number2;       //adding the two values in divide to print it
+int multi = number1*number2;        //adding the two values in multiplies to print it
+print('The Sum Is :$sum');          //printing the result of sum the two numbers
+print('the Subtract Is :$sub');     //printing the result of subtract the two numbers
+print('The Division Is :$div');     //printing the result of divide the two numbers
+print('The Multiplies Is :$multi'); //printing the result of <multiplies> the two numbers
+
+
+}
