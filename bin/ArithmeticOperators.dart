9d0090831b316012973f35858103c0aa43435698
Amr Yousre <master_j_20@hotmail.com>
2021-03-30 22:00:54 +0200

    adding same code with functions
    adding to way of using function ...
    1- using void
    2-using return data type

--- bin/ArithmeticOperators.dart
+++ bin/ArithmeticOperators.dart
@@ -20,15 +20,75 @@ void main() {
   // print(
   //     'The Multiplies Is :$multi'); //printing the result of <multiplies> the two numbers
 
-  // From Code 2 Start
-  // int x,y;
-  // x=5;
-  // y=2;
-  // print('$x + $y = ${(x+y)}');
-  // print('$x - $y = ${(x-y)}');
-  // print('$x * $y = ${(x*y)}');
-  // print('$x / $y = ${(x/y)}');
-  // print('$x ~/ $y = ${(x~/y)}');  // ~/ make the result int not double
-  // print('$x % $y = ${(x%y)}');
 
+
+
+
+
+
+
+
+
+
+  //Another Way Using Function
+  print('Please Enter Number1:'); //asking the user to enter the first number
+  num number1 = int.parse(stdin.readLineSync()); //putting the value that user entered in number1
+  print('Please Enter Number2:'); //asking the user to enter the first numbe2
+  num number2 = int.parse(stdin.readLineSync()); //putting the value that user entered in number2
+  print(calcIt(number1, number2));
+
+
+}
+//using void data type
+// void calcIt(num number1,num number2){        //Function To Take The Operator From User And depend on this mark do his math
+//   print('''Please Enter Your Operator:
+// 1-'+' Sum Two Numbers.
+// 2-'-' Subtract Two numbers.
+// 3-'*' Multiple Two Numbers.
+// 4-'/' Divide Two numbers.''');
+//   var choose = stdin.readLineSync();                           // initialization for the choose value
+//   switch (choose) {                                                   // check the choose the user entered and check which case
+//     case '+' :
+//       print('$number1 + $number2 = ${(number1+number2)}');
+//       break;
+//     case '-' :
+//       print('$number1 - $number2 = ${(number1-number2)}');
+//       break;
+//     case '*' :
+//       print('$number1 * $number2 = ${(number1*number2)}');
+//       break;
+//     case '/' :
+//       print('$number1 / $number2 = ${(number1/number2)}');
+//       break;
+//     default:                                                    // check the user if he entered a wrong option give him this error message
+//       print('Invalid Option!!!');
+
+
+//using return data type
+  num calcIt(num number1,num number2){        //Function To Take The Operator From User And depend on this mark do his math
+    num result;
+    print('''Please Enter Your Operator:                       
+1-'+' Sum Two Numbers.
+2-'-' Subtract Two numbers.
+3-'*' Multiple Two Numbers.
+4-'/' Divide Two numbers.''');
+    var choose = stdin.readLineSync();                           // initialization for the choose value
+    switch (choose) {                                                   // check the choose the user entered and check which case
+      case '+' :
+        result = number1 + number2;
+        break;
+      case '-' :
+        result = number1 - number2;
+        break;
+      case '*' :
+        result = number1 * number2;
+        break;
+      case '/' :
+        result = number1 / number2;
+        break;
+      default:                                                    // check the user if he entered a wrong option give him this error message
+        print('Invalid Option!!!');
+        break;
+    }
+    return result;                                                // to return the value with the result to print it
 }
